a11e1dde9ee257b1bf635281c254f28fbf6fded8
thom03 <thomas.muteti@gmail.com>
2023-02-23 18:56:24 +0300

    Refactoring Dataset page.

---
+++ resources/views/bundle.blade.php
@@ -0,0 +1,141 @@
+{{-- Extends layout --}}
+@extends('layout.landing')
+
+
+{{-- Content --}}
+@section('content')
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">11.4-Gender report-SRFSI Project-ACIAR-CIMMYT</h4>
+                </div>
+                <div class="card-body">
+
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-striped" style="min-width: 900px;">
+                            <thead>
+                            <tr>
+                                <th></th>
+                                <th class="text-center text-dark">
+                                    <br>
+                                    <small class="text-muted">&lt;</small>
+                                </th>
+
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <th class="text-nowrap text-dark" scope="row">Author(s)</th>
+                                <td>  Gathala, Mahesh K Tiwari, Thakur P Islam, Saiful Md. Ashraf Ali Maharjan, Sofina Das, Santu Gerard, Bruno Chowdhury, Apurba K Bhattacharya, Prateek M Das, K.K Dhar, Tapamay Pradhan, K Sinha, A.K Ghosh, Arunava Mitra, B Chattopadhyay, C Sen, Sujan K Chatteerjee, Rajat Ghosh, D Manbendra, T Sarkar Bhatt, B.P Kumar, Ujjwal Kumar, Ramesh Rao, Koteswara Khan, A.S.M Anwar, Mazharul Hossain, Illias Siddique, Nur-E-A Hossain, Akbar Hossain, Shakhawat Shamim Rashid, Mamunur Islam, Rashadul Ghosh, Anup K Hossain, Zakir Al Mamun, Abdulla Kumar, Sanjay Kumar, Ranveer Datt, Ram Dutta, S.K Tomar, J.B Singh, A.K Shrestha, Renuka Chaudhary, B Shrestha, H.K Manandhar, S Paneru, Prakesh Adhikari, S Shrestha, S.R Jha, S.K Thapa Magar, D.B Acharya, U Sapkota, B Pandey, Kanchan R Chaube, R Thakur, P Colvito, L Pradhan, Komal Mishra, G JEEViKA Sakhi Joshi, P.K Avinash, K Fraser, S Sanjiv, S. de Saikia, P Brown, Peter Gaydon, Don Laing, Alison Darbas, Toni Poulton, Perry Rubzen, Fay R Prior, Roy Dalal, R Menzies, N Prior, Julian McDonald, Andrew Aryal, J
+                                </td>
+
+                            </tr>
+                            <tr>
+                                <th class="text-nowrap text-dark" scope="row">Release year</th>
+                                <td><code>2018</code></td>
+
+                            </tr>
+                            <tr>
+                                <th class="text-nowrap text-dark" scope="row">Region</th>
+                                <td colspan="5">  Southern Asia Northern Europe Australia and New Zealand</td>
+                            </tr>
+                            <tr>
+                                <th class="text-nowrap text-dark" scope="row">Resource Files</th>
+                                <td colspan="5"></td>
+                            </tr>
+                            <tr>
+                                <th class="text-nowrap text-dark" scope="row">Source</th>
+                                <td colspan="5"> CIMMYT Research Data & Software Repository
+                                </td>
+                            </tr>
+                            <tr>
+                                <th class="text-nowrap text-dark" scope="row">Access</th>
+                                <td colspan="5">Open</td>
+                            </tr>
+                            <tr>
+                                <th class="text-nowrap text-dark" scope="row">Data Type</th>
+                                <td colspan="5">Quantitative(Social-economic and M&E data)
+                                </td>
+                            </tr>
+                            <tr>
+                                <th class="text-nowrap text-dark" scope="row">Production System</th>
+                                <td colspan="5">Rice</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Innovations</h4>
+                </div>
+                <div class="card-body">
+
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-striped" style="min-width: 490px;">
+                            <thead>
+                            <tr>
+                                <th></th>
+                                <th class="text-center text-dark">
+                                    <br>
+                                    <small class="text-muted">&lt;</small>
+                                </th>
+
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <th class="text-nowrap text-dark" scope="row">Social inclusion/protection</th>
+                                <td> No</td>
+
+                            </tr>
+                            <tr>
+                                <th class="text-nowrap text-dark" scope="row">Policy/Institutional</th>
+                                <td><code>Yes</code></td>
+
+                            </tr>
+                            <tr>
+                                <th class="text-nowrap text-dark" scope="row">Organizational</th>
+                                <td colspan="5"> </td>
+                            </tr>
+                            <tr>
+                                <th class="text-nowrap text-dark" scope="row">Marketing</th>
+                                <td colspan="5">Yes</td>
+                            </tr>
+                            <tr>
+                                <th class="text-nowrap text-dark" scope="row">Financial</th>
+                                <td colspan="5"> No
+                                </td>
+                            </tr>
+                            <tr>
+                                <th class="text-nowrap text-dark" scope="row">Insurance</th>
+                                <td colspan="5">Yes</td>
+                            </tr>
+                            <tr>
+                                <th class="text-nowrap text-dark" scope="row">Digital</th>
+                                <td colspan="5">Yes</td>
+                            </tr>
+                            <tr>
+                                <th class="text-nowrap text-dark" scope="row">Training</th>
+                                <td colspan="5">Yes</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -71,26 +71,26 @@
                 </div>
             </div>
         </div>
-        <div class="col-xl-3 col-lg-6 col-sm-6">
-            <div class="widget-stat card">
-                <div class="card-body p-4">
-                    <div class="media ai-icon">
-									<span class="mr-3 bgl-success text-success">
-										<svg id="icon-database-widget" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-database">
-											<ellipse cx="12" cy="5" rx="9" ry="3"></ellipse>
-											<path d="M21 12c0 1.66-4 3-9 3s-9-1.34-9-3"></path>
-											<path d="M3 5v14c0 1.66 4 3 9 3s9-1.34 9-3V5"></path>
-										</svg>
-									</span>
-                        <div class="media-body">
-                            <p class="mb-1">Patient</p>
-                            <h4 class="mb-0">364</h4>
+{{--        <div class="col-xl-3 col-lg-6 col-sm-6">--}}
+{{--            <div class="widget-stat card">--}}
+{{--                <div class="card-body p-4">--}}
+{{--                    <div class="media ai-icon">--}}
+{{--									<span class="mr-3 bgl-success text-success">--}}
+{{--										<svg id="icon-database-widget" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-database">--}}
+{{--											<ellipse cx="12" cy="5" rx="9" ry="3"></ellipse>--}}
+{{--											<path d="M21 12c0 1.66-4 3-9 3s-9-1.34-9-3"></path>--}}
+{{--											<path d="M3 5v14c0 1.66 4 3 9 3s9-1.34 9-3V5"></path>--}}
+{{--										</svg>--}}
+{{--									</span>--}}
+{{--                        <div class="media-body">--}}
+{{--                            <p class="mb-1">Patient</p>--}}
+{{--                            <h4 class="mb-0">364</h4>--}}
 {{--                            <span class="badge badge-success">-3.5%</span>--}}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
 
         <div class="row">
             <div class="col-12">
