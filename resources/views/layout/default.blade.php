895d8da5d7e50ba0d76afcdbc998c33ae896d42a
thom03 <thomas.muteti@gmail.com>
2023-03-07 21:29:22 +0300

    Added status message on the system.

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -104,6 +104,23 @@
             Content body start
         ***********************************-->
         <div class="content-body">
+            @if(session('status'))
+                <div class="col-xl-6">
+                    <div class="alert alert-success left-icon-big alert-dismissible fade show">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span><i class="mdi mdi-close"></i></span>
+                        </button>
+                        <div class="media">
+                            <div class="alert-left-icon-big">
+                                <span><i class="mdi mdi-check-circle-outline"></i></span>
+                            </div>
+                            <div class="media-body">
+                                <h5 class="mt-1 mb-2">Success!</h5>
+                                <p class="mb-0">{{ session('status') }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endif
             <!-- row -->
             @yield('content')
         </div>
@@ -140,5 +157,6 @@
         Scripts
     ***********************************-->
 	@include('elements.footer-scripts')
+
 </body>
 </html>
