142e2b8a8e7ebdbd42ea54d3624660f0d7e947e7
thom03 <thomas.muteti@gmail.com>
2023-07-16 19:59:41 +0300

    Added About page.

---
+++ resources/views/about.blade.php
@@ -0,0 +1,38 @@
+{{-- Extends layout --}}
+@extends('layout.landing')
+
+
+{{-- Content --}}
+@section('content')
+    <div class="page-wrapper">
+        <div class="container-fluid">
+            <div class="row justify-content-center"> <!-- Add 'justify-content-center' class to center the row -->
+                <div class="col-md-6"> <!-- Set the desired column width -->
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="col-md-12">
+                                <h1 class="text-center">About Us!</h1>
+                                <h2>Social-Technical Innovation Bundle (STIB) Toolbox</h2>
+                                <p class="text-justify">xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</p>
+                            </div>
+                            <div class="col-md-12">
+                                <h2 class="m-b-0">Concept by</h2>
+                                <a href="#" target="_blank"><b>Dr. Eileen Nchanji </b></a>xxxxxxxx<br />
+                                <a href="#" target="_blank"><b>Leroy Mwanzia</b></a> Senior Data Manager<br />
+                                <a href="#" target="_blank"><b>Cosmus Lutomia</b></a> xxxxxxxx<br />
+                                <a href="#" target="_blank"><b>George Thomas Muteti</b></a> Data Systems Analyst<br />
+
+                                <br />
+                                <h2 class="m-b-0">Credits</h2>
+                                <a href="https://ciat.cgiar.org/" target="_blank"><img src="https://soecodat.ciat.cgiar.org/images/logos/ciat.png" width="175"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+@endsection
